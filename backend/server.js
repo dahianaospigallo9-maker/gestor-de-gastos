96b2cf04b69811a758aee84ff01b1e02598fe946
Dahiana <dahianaospigallo9@gmail.com>
2026-08-27 23:42:26 +0200

    Configurar puerto para despliegue

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const loginRoutes = require("./routes/login");
 const movimientosRoutes = require("./routes/movimientos");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // CONFIGURACIÓN
 
